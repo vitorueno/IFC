abb1949770df4ca4307aad78400296dc4fce91aa
vitorueno <vitoruenootto@gmail.com>
2020-08-27 14:48:23 -0300

    Add create book functionality in the frontend

--- sistema-livros/back-end/config.py
+++ sistema-livros/back-end/config.py
@@ -1,12 +1,13 @@
 # importações
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 import os
 
 
 # configuration
 app = Flask(__name__)
-
+CORS(app)
 # finding the path to the db file
 path = os.path.dirname(os.path.abspath(__file__)) # directory path
 db_file = os.path.join(path, 'books.db') # db file path
--- sistema-livros/front-end/js/script.js
+++ sistema-livros/front-end/js/script.js
@@ -1,25 +1,91 @@
-$(function() {
+$(function () {
+  changePage('inicial');
+  $('#link-listar').click(function () {
     $.ajax({
-        url: 'http://localhost:5000/get-books',
-        method: 'GET',
-        dataType: 'json', // we expect to receive a json response
-        success: listBooks, // calling the main function
-        error: function() {
-            alert("erro ao ler dados, verifique o backend");
-        }
+      url: 'http://localhost:5000/get-books',
+      method: 'GET',
+      dataType: 'json', // we expect to receive a json response
+      success: listBooks, // calling the main function
+      error: function () {
+        alert('erro ao ler dados, verifique o backend');
+      },
+    });
+  });
+
+  $('#link-inicial').click(function () {
+    changePage('inicial');
+  });
+
+  $('#nav-brand').click(function () {
+    changePage('inicial');
+  });
+
+  $('#btn-incluir').click(function () {
+    const titulo = $('#campo-titulo').val();
+    const autor = $('#campo-autor').val();
+    const descricao = $('#campo-descricao').val();
+    const genero = $('#campo-genero').val();
+    const editora = $('#campo-editora').val();
+
+    const bookData = JSON.stringify({
+      title: titulo,
+      author: autor,
+      description: descricao,
+      genre: genero,
+      publisher: editora,
     });
 
-    function listBooks(books) {
-        for (book of books) { // creating a line for each book in our backend
-            newRow = `<tr> 
+    $.ajax({
+      url: 'http://localhost:5000/create-books',
+      type: 'POST',
+      contentType: 'application/json',
+      dataType: 'json',
+      data: bookData,
+      success: createBook,
+      error: createBookError,
+    });
+  });
+
+
+  function createBook(resposta) {
+    if (resposta.result == 'ok') {
+        alert('Livro adicionado com sucesso')
+        $('#campo-titulo').val('');
+        $('#campo-autor').val('');
+        $('#campo-descricao').val('');
+        $('#campo-genero').val('');
+        $('#campo-editora').val('');
+    } else {
+        alert('Erro na adição do livro!')
+    }
+  }
+
+  function createBookError(resposta){
+    alert('Erro na chamada do back-end')
+  }
+
+  function listBooks(books) {
+    var rows = '';
+
+    for (book of books) {
+      // creating a line for each book in our backend
+      newRow = `<tr> 
                         <td>${book.title}</td> 
                         <td>${book.author}</td> 
                         <td>${book.description}</td> 
                         <td>${book.genre}</td> 
                         <td>${book.publisher}</td> 
                       </tr>`;
-            // adding the new book to the html
-            $('#tableBody').append(newRow); 
-        }
+      rows += newRow;
+      // adding the new book to the html
+      $('#tableBody').html(rows);
     }
+    changePage('listar');
+  }
+
+  function changePage(nextPage) {
+    $('#container-inicial').addClass('invisible');
+    $('#container-listar').addClass('invisible');
+    $(`#container-${nextPage}`).removeClass('invisible');
+  }
 });
