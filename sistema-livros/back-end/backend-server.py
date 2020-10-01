486cfdf1fb2b39e8e496ba694abff8a8d76d8720
vitorueno <vitoruenootto@gmail.com>
2020-10-01 10:31:14 -0300

    Add delete route in backend

--- sistema-livros/back-end/backend-server.py
+++ sistema-livros/back-end/backend-server.py
@@ -2,7 +2,7 @@ from config import *
 from models import Book
 
 
-@app.route('/get-books', methods=['get'])
+@app.route('/get-books', methods=['GET'])
 def get_books():
     db_books = db.session.query(Book).all()
     json_books = [ book.json() for book in db_books ]
@@ -10,7 +10,7 @@ def get_books():
     response.headers.add("Access-Control-Allow-Origin", "*")
     return response
 
-@app.route('/create-books', methods=['post'])
+@app.route('/create-books', methods=['POST'])
 def create_book():
     response = jsonify({"status": "201", "result": "ok", "details": "Book created"})
     data = request.get_json()
@@ -23,5 +23,17 @@ def create_book():
     response.headers.add("Access-Control-Allow-Origin", "*")
     return response 
 
+@app.route('/delete-books/<int:id>', methods=['DELETE'] )
+def delete_books(id):
+    response = jsonify({"status": "200", "result": "ok", "details": "Book deleted"})
+    try:
+        Book.query.filter(Book.id == id).delete()
+        db.session.commit()
+    except Exception as e:
+        resposta = jsonify({"status": "400" ,"result": "error", "details": str(e)})
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
+    
+    
 if __name__ == '__main__':
     app.run(debug=True)
--- sistema-livros/front-end/js/script.js
+++ sistema-livros/front-end/js/script.js
@@ -1,31 +1,31 @@
 $(function () {
-  changePage('inicial');
-  $('#link-listar').click(function () {
+  changePage("inicial");
+  $("#link-listar").click(function () {
     $.ajax({
-      url: 'http://localhost:5000/get-books',
-      method: 'GET',
-      dataType: 'json', // we expect to receive a json response
+      url: "http://localhost:5000/get-books",
+      method: "GET",
+      dataType: "json", // we expect to receive a json response
       success: listBooks, // calling the main function
       error: function () {
-        alert('erro ao ler dados, verifique o backend');
+        alert("erro ao ler dados, verifique o backend");
       },
     });
   });
 
-  $('#link-inicial').click(function () {
-    changePage('inicial');
+  $("#link-inicial").click(function () {
+    changePage("inicial");
   });
 
-  $('#nav-brand').click(function () {
-    changePage('inicial');
+  $("#nav-brand").click(function () {
+    changePage("inicial");
   });
 
-  $('#btn-incluir').click(function () {
-    const titulo = $('#campo-titulo').val();
-    const autor = $('#campo-autor').val();
-    const descricao = $('#campo-descricao').val();
-    const genero = $('#campo-genero').val();
-    const editora = $('#campo-editora').val();
+  $("#btn-incluir").click(function () {
+    const titulo = $("#campo-titulo").val();
+    const autor = $("#campo-autor").val();
+    const descricao = $("#campo-descricao").val();
+    const genero = $("#campo-genero").val();
+    const editora = $("#campo-editora").val();
 
     const bookData = JSON.stringify({
       title: titulo,
@@ -36,56 +36,55 @@ $(function () {
     });
 
     $.ajax({
-      url: 'http://localhost:5000/create-books',
-      type: 'POST',
-      contentType: 'application/json',
-      dataType: 'json',
+      url: "http://localhost:5000/create-books",
+      type: "POST",
+      contentType: "application/json",
+      dataType: "json",
       data: bookData,
       success: createBook,
       error: createBookError,
     });
   });
 
-
   function createBook(resposta) {
-    if (resposta.result == 'ok') {
-        alert('Livro adicionado com sucesso')
-        $('#campo-titulo').val('');
-        $('#campo-autor').val('');
-        $('#campo-descricao').val('');
-        $('#campo-genero').val('');
-        $('#campo-editora').val('');
+    if (resposta.result == "ok") {
+      alert("Livro adicionado com sucesso");
+      $("#campo-titulo").val("");
+      $("#campo-autor").val("");
+      $("#campo-descricao").val("");
+      $("#campo-genero").val("");
+      $("#campo-editora").val("");
     } else {
-        alert('Erro na adição do livro!')
+      alert("Erro na adição do livro!");
     }
   }
 
-  function createBookError(resposta){
-    alert('Erro na chamada do back-end')
+  function createBookError(resposta) {
+    alert("Erro na chamada do back-end");
   }
 
   function listBooks(books) {
-    var rows = '';
+    var rows = "";
 
     for (book of books) {
       // creating a line for each book in our backend
       newRow = `<tr> 
-                        <td>${book.title}</td> 
-                        <td>${book.author}</td> 
-                        <td>${book.description}</td> 
-                        <td>${book.genre}</td> 
-                        <td>${book.publisher}</td> 
-                      </tr>`;
+                  <td>${book.title}</td> 
+                  <td>${book.author}</td> 
+                  <td>${book.description}</td> 
+                  <td>${book.genre}</td> 
+                  <td>${book.publisher}</td> 
+                </tr>`;
       rows += newRow;
       // adding the new book to the html
-      $('#tableBody').html(rows);
+      $("#tableBody").html(rows);
     }
-    changePage('listar');
+    changePage("listar");
   }
 
   function changePage(nextPage) {
-    $('#container-inicial').addClass('invisible');
-    $('#container-listar').addClass('invisible');
-    $(`#container-${nextPage}`).removeClass('invisible');
+    $("#container-inicial").addClass("invisible");
+    $("#container-listar").addClass("invisible");
+    $(`#container-${nextPage}`).removeClass("invisible");
   }
 });
